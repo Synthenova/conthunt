5372e3ff0f5b2357f818b5bf630aec0629d0e702
Nirmal2000 <nirmalvelu2000@gmail.com>
2026-01-20 05:37:55 +0530

    suspense

--- frontend/src/app/app/layout.tsx
+++ frontend/src/app/app/layout.tsx
@@ -1,6 +1,7 @@
 import { AppShell } from "@/components/layout/AppShell";
 import { ProductsProvider } from "@/contexts/ProductsContext";
 import { TutorialProvider, TutorialTooltip } from "@/components/tutorial";
+import { Suspense } from "react";
 
 export default function AppLayout({
     children,
@@ -9,10 +10,12 @@ export default function AppLayout({
 }) {
     return (
         <ProductsProvider>
-            <TutorialProvider>
-                <AppShell>{children}</AppShell>
-                <TutorialTooltip />
-            </TutorialProvider>
+            <Suspense fallback={<div className="min-h-screen" />}>
+                <TutorialProvider>
+                    <AppShell>{children}</AppShell>
+                    <TutorialTooltip />
+                </TutorialProvider>
+            </Suspense>
         </ProductsProvider>
     );
 }
